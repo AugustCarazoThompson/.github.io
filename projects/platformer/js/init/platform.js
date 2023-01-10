d61ad5cf76a67bfc001586b25b8852757ee5da17
AugustCarazoThompson <itselvisosceola@gmail.com>
2023-01-10 19:32:29 +0000

    OHIO

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -38,7 +38,10 @@
 
     // example:
     createPlatform(400, 250);
-    createPlatform(170, 460, 0.5);
+    createPlatform(0, 600, 0.5);
+    createPlatform(0, 500, 0.5);
+    createPlatform(0, 400, 0.5);
+    createPlatform(0, 300, 0.5);
 
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
