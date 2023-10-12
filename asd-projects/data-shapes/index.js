d7d7568b6b19f93294233795336f173d518f6ce0
AugustCarazoThompson <itselvisosceola@gmail.com>
2023-10-12 15:34:24 +0000

    hi 2\

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -36,52 +36,49 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO BELOW HERE ////////
   /////////////////////////////////////////////////
-<<<<<<< HEAD
+
 "// TODO 0 complete"
   // TODO 1: create a new shape object and add it to the array
   var shape = {
-color: "blue",
-shape: "circle",
-repeat: 3
+  color: "blue",
+  shape: "circle",
+  repeat: 3
   }
 dataShapes.push(shape)
-=======
   // TODO 2: add a new property to all data shapes
   for(i = 0; i < dataShapes.length; i++){
-var currentShape = dataShapes[i];
-if (currentShape.color === "red"){
-  currentShape.goodBehavior = "bounce"
-}
-else if (currentShape.color === "blue"){
-  currentShape.goodBehavior = "blink"
-}
-else {
-  currentShape.goodBehavior = "spin"
-}
+    var currentShape = dataShapes[i];
+    if(currentShape.color === "red"){
+      currentShape.goodBehavior = "bounce"
+    }
+    else if(currentShape.color === "blue"){
+      currentShape.goodBehavior = "blink"
+    }
+    else{
+      currentShape.goodBehavior = "spin"
+    }
+   
   }
-
+  
   // TODO 3-a: add a function that handles the static display type
   function handleStatic(data){
-<<<<<<< HEAD
-setBackgroundWithObject(data);
-animationDetails.displayType = 1;
-=======
-setBackgroundWithDataObject(data);
-animationDeatails.displayType = 1;
+    setBackgroundWithObject(data);
+    animationDetails.displayType = 1;
   }
 
   // TODO 4-a: add a function that handles the good display type
-  function handleGood(color,shape,repeat){
-    setBackgroundWithSimple(color, shape, repeat);
-    animationDetails.displayType = 2;
-      }
+ function handleGood (color, shape, repeat){
+  setBackgroundWithSimple(color, shape, repeat);
+  animationDetails.displayType = 2;
+ }
 
   // TODO 5-a: add a function that handles the bad display type
-  function handleBad(data,repeat)){
-    repeat +=1;
-    setBackgroundWithMixed(data, repeat);
-    animationDetails.displayType = 3;
-      }
+ function handleBad (data, repeat){
+  repeat +=1;
+  setBackgroundWithMixed(data, repeat);
+  animationDetails.displayType = 3;
+ }
+  
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -94,8 +91,8 @@ animationDeatails.displayType = 1;
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    var currentShape = dataShapes[currentIndex];
-    handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
+ var currentShape = dataShapes[currentIndex];
+ handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
   }
 
   function badDisplay() {
@@ -104,7 +101,6 @@ animationDeatails.displayType = 1;
     var repeat= currentShape.repeat;
     handleBad(currentShape, repeat)
   }
-
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO ABOVE HERE ////////
   /////////////////////////////////////////////////
